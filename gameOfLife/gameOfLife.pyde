f94b9f5b50a4f3ba3e7c80cc26877f7845723cab
Rithik <rithikrathan.c@gmail.com>
2025-12-12 20:14:01 +0530

    hope this works

---
+++ gameOfLife/gameOfLife.pyde
@@ -0,0 +1,69 @@
+import random
+
+rows = 84
+cols = 48
+cellSize = 10
+showGrid = True
+
+currGen = []
+
+
+def neighbourCount(cellx, celly):
+    global currGen
+    sum = 0
+
+    # (-1,1)    (0, 1)   (1,1)
+    # (-1,0)    (0, 0)   (1,0)
+    # (-1,-1)  (0, -1)  (1,-1)
+
+    for i in (-1, 0, 1):
+        for j in (-1, 0, 1):
+            sum += currGen[(cellx + i) % rows][(celly - j) % cols]
+
+    sum -= currGen[cellx][celly]
+
+    return sum
+
+
+def setup():
+    global currGen
+    size(840, 480)
+    currGen = [[0] * cols for _ in range(rows)]
+
+    # for i in range(0, rows):
+    #     for j in range(0, cols):
+    #         currGen[i][j] = random.randint(0, 1)
+
+    list = [(400, 400)]
+
+    for x, y in list:
+        currGen[x][y] = 1
+
+
+def draw():
+    global currGen
+    nextGen = [[0]*cols for _ in range(rows)]
+
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if currGen[i][j]:
+                fill(255, 255, 255)
+            else:
+                fill(0, 0, 0)
+            stroke(100)
+            square(i * cellSize, j * cellSize, cellSize)
+
+            # actual logic for the game of life
+            count = neighbourCount(i, j)
+            if currGen[i][j]:
+                if count in (2, 3):  # survives
+                    nextGen[i][j] = currGen[i][j]
+                elif count < 2:  # dies of underpopulation
+                    nextGen[i][j] = 0
+                elif count > 3:  # dies of overpopulation
+                    nextGen[i][j] = 0
+            else:
+                if count == 3:
+                    nextGen[i][j] = 1  # birth by reproduction
+
+    currGen = nextGen
---
+++ proceduralKolams/simpleKolam/reference/simpleKolam.java
@@ -0,0 +1,192 @@
+import com.krab.lazy.*;
+
+import processing.core.*;
+import processing.data.*;
+import processing.event.*;
+import processing.opengl.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+/** Processing Sketch simpleKolam */
+public class simpleKolam {
+
+LazyGui gui;
+
+float radialSubdivision  = 6;
+float matrixDensity  = 27;
+PVector scaleFactor = new PVector(200, 200);
+PVector origin;
+
+//boolean variables
+bool evenMatrix = false;
+
+ArrayList<vertex> polygonVertices = new ArrayList<vertex>();
+ArrayList<vertex> guidePoints = new ArrayList<vertex>();
+
+class vertex {
+    float x, y;
+    vertex(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+void handleGui(LazyGui gui) {
+    // lazy controls — created automatically first time run
+    radialSubdivision = gui.slider("radialSubdivision", radialSubdivision, 0, 10);
+    matrixDensity = gui.slider("matrixDensity", matrixDensity, 0, 100);
+    scaleFactor  = gui.plotXY("scaleFactor", scaleFactor.x, scaleFactor.y);
+    origin  = gui.plotXY("origin", width /2, height/2);
+
+    if (gui.button("resetValues")) {
+        radialSubdivision  = 6;
+        gui.sliderSet("radialSubdivision", 6);
+
+        matrixDensity  = 27;
+        gui.sliderSet("matrixDensity", 27);
+
+        scaleFactor.set(200, 200);
+        gui.plotSet("scaleFactor", 200, 200);
+
+        origin.set(width/2, height/2);
+        gui.plotSet("origin", width/2, height/2);
+
+        // recalculate when reset
+        polygonVertices.clear();
+        polygonVertices = calculateShape(polygonVertices);
+
+        guidePoints.clear();
+        guidePoints = calculateGuidePoints(guidePoints, evenMatrix);
+    }
+
+    if (gui.button("Quit")) {
+        exit();
+    }
+}
+
+ArrayList<vertex> calculateShape(ArrayList<vertex> pv) {
+    ArrayList<vertex> polygonVertices = pv;	
+    // calculate shape
+    for (int i = 0; i < PApplet.parseInt(radialSubdivision); i++) {
+        float theta = i * TAU / radialSubdivision;
+        float x = scaleFactor.x * cos(theta);
+        float y = scaleFactor.y * sin(theta);
+        polygonVertices.add(new vertex(x, y));
+    }
+    return polygonVertices;
+}
+
+ArrayList<vertex> calculateGuidePoints(ArrayList<vertex> gp, bool evenMatix) {
+    ArrayList<vertex> guidePoints = gp;	
+    // calculate guidePoints
+
+    return guidePoints;
+}
+
+bool isInBetween(float y, float y1, float y2) {
+    if ((y1 > y2 && y1 >= y > y2) || (y2>y1 && y2 >= y > y1) ) {
+        return true;
+    }
+    return false;
+}
+
+vertex getIntersection(vertex point, vertex a, vertex b) {
+    vertex pointOfIntersection;
+    try {
+        float slope = (b.y - a.y)/(b.x -a.x);
+        pointOfIntersection.y = point.y;
+        pointOfIntersection.x = ((pointOfIntersection.y - a.y)/slope)+a.x;
+        return pointOfIntersection;
+    }
+    catch (ArithmeticException e) {
+        pointOfIntersection.y = point.y;
+        pointOfIntersection.x = a.x;
+        return pointOfIntersection;
+        System.out.println("encountered arithmetic exception");
+    }
+}
+
+bool containedIn(vertex point, ArrayList<vertex> polygon) {
+    int count = 0;
+    for (int i = 0; i < polygonVertices.size(); i++) {
+        vertex vert_a = polygonVertices.get(i); //current vertex
+        vertex vert_b = polygonVertices.get((i+1) % polygonVertices.size()); // next vertex
+
+        if (vert_a.x != vert_b.x && isInBetween(point.y, vert_a.y, vert_b.y)) {
+            vertex pointOfIntersection = getIntersection(point, vert_a, vert_b);
+            if (pointOfIntersection.x > point.x) {
+                count ++;
+            }
+        }
+    }
+
+    if (count % 2 == 0) {
+        return false;
+    }
+
+    return true;
+}
+
+void setup() {
+    size(600, 600, P2D);
+    gui = new LazyGui(this); // init LazyGui
+
+    //calculate with the initial values
+    polygonVertices = calculateShape(polygonVertices);
+    guidePoints = calculateGuidePoints(guidePoints, evenMatrix);
+}
+
+void draw() {
+    background(30);
+    handleGui(gui);
+    translate(origin.x, origin.y); // set origin (0,0) to the middle of the screen
+
+    // recalculate when values changes
+    if (gui.hasChanged("radialSubdivision") || gui.hasChanged("scaleFactor") || gui.hasChanged("origin")) {
+        polygonVertices.clear();
+        polygonVertices = calculateShape(polygonVertices);
+    }
+
+    if (gui.hasChanged("matrixDensity")) {
+        guidePoints.clear();
+        guidePoints = calculateGuidePoints(guidePoints, evenMatrix);
+    }
+
+
+    for (int i = 0; i < polygonVertices.size(); i++) {
+        vertex vert_i = polygonVertices.get(i); //current vertex
+        vertex vert_j = polygonVertices.get((i+1) % polygonVertices.size()); // next vertex
+
+        // show edges
+        stroke(100, 179, 100);
+        strokeWeight(2);
+        line(vert_i.x, vert_i.y, vert_j.x, vert_j.y);
+
+        // show segments
+        stroke(100, 100, 200);
+        strokeWeight(1);
+        line(0, 0, vert_j.x, vert_j.y);
+
+        // show vertices
+        stroke(0, 255, 0);
+        strokeWeight(7);
+        point(vert_i.x, vert_i.y);
+    }
+
+    // draw logic
+    stroke(255, 0, 0);
+    strokeWeight(5);
+    point(0, 0); // mark origin
+
+    gui.draw(); // draw the GUI
+}
+
+}
+
